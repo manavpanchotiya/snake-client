878774de2c05d775d6377ea47fda3731dd4d6786
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-09 15:35:35 -0500

    added initial client code

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.255.255.254',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on ("connect", () => {
+    console.log("Connection established!");
+
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
