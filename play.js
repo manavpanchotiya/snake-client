d6a00c2d157e26cfd6796c44a9b1845919ab8b8a
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-09 15:38:52 -0500

    ip address correction

--- play.js
+++ play.js
@@ -1,17 +1,17 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: '10.255.255.254',
+    host: 'localhost',
     port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on ("connect", () => {
-    console.log("Connection established!");
+  conn.on('data', (data) => {
+    console.log('Server says:',data);
 
   });
 
