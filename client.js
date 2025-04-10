309b184551c4fc6843143f40692b8d7e0b778e89
manavpanchotiya <manavpanchotiya@gmail.com>
2025-04-09 19:45:42 -0500

    constant reassign

--- client.js
+++ client.js
@@ -1,16 +1,17 @@
 const net = require("net");
-
+const {IP, PORT} = require("./constants")
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server.");
+    console.log("Name: MP");
   
     conn.write("Name: SNK");
     // conn.write("Move: Up");
